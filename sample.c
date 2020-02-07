71329f6061049e7505fb8cd4938cd87caf40a886
Nareshdevops12 <naresh2377.maruthi@gmail.com>
2020-02-07 10:52:51 +0000

    Arith operation

--- sample.c
+++ sample.c
@@ -9,3 +9,31 @@ class Bank
     bal=bal-amt
   }
 }
+
+================================================================================================
+
+void division()
+{
+   c=a/b
+}
+void Multiplication()
+{
+   c=a*b
+}
+ void Addition()
+{
+  C=A+B
+}
+void Subtraction()
+{
+  C=A-B
+}
+class demo
+{
+  public static void main()
+  {
+      printf(" good day");
+      printf(" good day");
+      printf(" good day");
+  }
+}
